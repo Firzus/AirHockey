6220a4b63a5ed866c88f1bd9f1567908d17b5036
Firzus <92395888+Firzus@users.noreply.github.com>
2023-09-15 13:56:47 +0200

    add all content menu

--- Menu.h
+++ Menu.h
@@ -1,5 +1,30 @@
 #pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
 class Menu
 {
+private:
+	sf::RectangleShape background;
+
+	sf::Font fontBlack;
+	sf::Font fontBold;
+	sf::Font fontMedium;
+
+	sf::Text titleText;
+	sf::Text namestext;
+	sf::Text clickStartText;
+	
+	sf::Texture paletTexture;
+	sf::Sprite paletSprite;
+
+public:
+	Menu();
+
+	void Render(sf::RenderWindow& rw);
+	
+	void LoadFont();
+	void LoadSprite();
 };
 
